a593903308c1fe75d3f9ecbf972da79d60c3628b
Amin <Amin@Amin-PC>
2017-04-07 17:12:42 -0400

    Fixed python version problem with subprocess.communicate(..)

--- digraph.py
+++ digraph.py
@@ -1,9 +1,20 @@
 # this module implements the dag part of lexis project
 import subprocess
-cprocess = subprocess.Popen(["./suffixtree/Suffix.exe"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+
+
+
+class CoreDAG(object):
+    n = []
+
+cprocess = subprocess.Popen(["./suffixtree/Suffix.exe"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
 
 # need to convert sequence chars into integer sequence so that a new symbol can be added always
-while cprocess.poll()==None:
-    print cprocess.communicate("aabcdeaabxfd")[0]      #communicates param passes string to stdin of child process
 
-print "done finding maximal repeats"
+#cprocess.stdin.write("aabcdeaabxfd")
+while cprocess.poll()==None:
+    print (cprocess.communicate(input="aabcabaabxyzaab")[0])      #communicates param passes string to stdin of child process
+
+print ("done finding maximal repeats")
+
+
