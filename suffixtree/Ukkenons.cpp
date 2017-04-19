13afd7f6b183d84bfc33160808813f2f650512fd
Amin <Amin@Amin-PC.fios-router.home>
2017-04-19 11:38:45 -0400

    commit: implemented linear time ukkenon's algorithm for suffix tree

--- suffixtree/Ukkenons.cpp
+++ suffixtree/Ukkenons.cpp
@@ -5,21 +5,24 @@ ActivePoint _ap;
 #define aedge _ap.active_edge
 #define alen _ap.active_len
 
-int _end = -1;		// this value will be the second item of each edge's label pair
+_End *_end = new _End(-1);		// this value will be the second item of each edge's label pair
 int _suffixToBeAdded = 0;
 _tnode *root;
 _vint input_vect;
 
 
+
 int getNextElement(int );
 void vPrint(_vint v){
 	for(auto i:v) cout<<" "<<i;
-
+}
+void vPrint(_vint v, _pair p){
+	for(int i=p.first;i<=p.second;i++) cout<<" "<<v[i];
 }
 
 void testGetNextElement(){
 	// unit test function to test getNextElement method
-
+	/*
 	// set up
 	int input_seq[] = {1,2,3,1,2,4,1};
 	input_vect.assign(input_seq, input_seq+sizeof(input_seq)/sizeof(int));
@@ -33,18 +36,20 @@ void testGetNextElement(){
 	alen = 2;
 	aedge = 0;
 	cout<<"next element should be 3, we get "<<getNextElement(5)<<endl;
-
+*/
 }
+
 int getNextElement(int curr_pos){
 	int curr_element = input_vect[curr_pos];
-	//cout<<curr_element<<endl;
-	//return 0;
+
+
 	_tnode *nodeDir = anode->childs[input_vect[aedge]];
-	int edgeLen =  (nodeDir->edge_label.second-nodeDir->edge_label.first);
-	//cout<<"edge len"<<edgeLen<<endl;
-	//cout<<"alen"<<alen<<endl;
+
+	int edgeLen =  (nodeDir->_end->end_val-nodeDir->_start);
+	cout<<"\ninside getnextel "<<nodeDir->_start<<" "<<edgeLen<<" "<<alen<<" "<<curr_element<<" "<<aedge<<endl;
+
 	if(edgeLen>=alen){
-		return input_vect[nodeDir->edge_label.first+alen];
+		return input_vect[nodeDir->_start+alen];
 	}
 	if((edgeLen+1) == alen){
 		if(nodeDir->childs.find(curr_element)!=nodeDir->childs.end()){
@@ -54,26 +59,40 @@ int getNextElement(int curr_pos){
 	}
 	else{
 		// updating active points
+		//cout<<"yeah um here"<<endl;
+		int tempanodestart = anode->_start;
 		anode = nodeDir;
+		//int tempalen = alen;
 		alen = alen - edgeLen-1;
-		aedge = aedge + alen + 1;
+		aedge = aedge + edgeLen + 1;
+		//aedge =  edgeLen + tempalen + 1;
+		//aedge = aedge + tempalen + 1;
+		//aedge = aedge + tempalen;
+		//nodeDir = anode->childs[input_vect[aedge]];
+		//if(nodeDir==NULL) cout<<"this"<<endl;
+		//cout<<"foitta before return "<<anode->_start<<" "<<alen<<"  "<<aedge<<endl;
 		return getNextElement(curr_pos);
 	}
 }
 void buildPhases(int start_pos){
-	_end++;
+	_end->end_val++;
 	_tnode *lastIntNode = 0;
 	_suffixToBeAdded++;
-
+	cout<<"end val "<<_end->end_val<<" ";
 	while(_suffixToBeAdded > 0){
 		if(_ap.active_len==0){
-			if(_ap.active_node->childs.find(input_vect[start_pos])!=_ap.active_node->childs.end()){
-				_ap.active_edge = _ap.active_node->childs[input_vect[start_pos]]->edge_label.first;
+			if(anode->childs.find(input_vect[start_pos])!=anode->childs.end()){
+				cout<<"if if"<<endl;
+				// show stopper
+				_ap.active_edge = anode->childs[input_vect[start_pos]]->_start;
 				_ap.active_len++;
+				cout<<" active edge "<<aedge<<"alen "<<alen<<endl;
 				break;
 			}
 			else{
-				_tnode *newChild = new _tnode({start_pos,_end});
+				cout<<"if else"<<endl;
+				_tnode *newChild = new _tnode(start_pos,_end);
+				root->addChild(input_vect[start_pos],newChild);
 				_suffixToBeAdded--;
 			}
 		}
@@ -81,17 +100,105 @@ void buildPhases(int start_pos){
 			// getting the next element in case active len not 0
 			int nextElm = getNextElement(start_pos);
 
-			if(nextElm==input_vect[start_pos]){
+
+
+			if(nextElm==-1){	// a leaf node need to be added here
+				cout<<"else if -1"<<endl;
+				_tnode *newLeaf = new _tnode(start_pos,_end);
+				anode->childs[input_vect[aedge]]->addChild(input_vect[start_pos],newLeaf);
 
 				if(lastIntNode){
-					lastIntNode->suffix_link = anode[input_vect[aedge]];
+					lastIntNode->suffix_link = anode;
 				}
+
+				// just made anode an internal one
+				lastIntNode = anode;
+
+				if(anode==root){
+					aedge++;
+					alen--;
+				}
+				else{
+					anode = anode->suffix_link;
+				}
+
+				_suffixToBeAdded--;
+				cout<<" active edge "<<aedge<<"alen "<<alen<<endl;
+				continue;
+			}
+
+			if(nextElm==input_vect[start_pos]){
+				cout<<"else if"<<endl;
+				if(lastIntNode){
+					lastIntNode->suffix_link = anode->childs[input_vect[aedge]];
+				}
+
+				// updating active len and other things
+				_tnode *currNode = anode->childs[input_vect[aedge]];
+				cout<<"comparison"<<currNode->_end->end_val<<"-"<<currNode->_start<<" "<<alen<<endl;
+
+				if(currNode->_end->end_val-currNode->_start < alen){
+					anode = currNode;
+					alen = alen - (currNode->_end->end_val-currNode->_start);
+					aedge = currNode->childs[input_vect[start_pos]]->_start;
+				}
+				else {
+					alen++;
+				}
+				cout<<" active edge "<<aedge<<"alen "<<alen<<endl;
+				break;
 			}
 			else{
+				cout<<"else else"<<endl;
+				_tnode *currNode = anode->childs[input_vect[aedge]];
 
+				int currentStart = currNode->_start;
+				currNode->_start = currNode->_start+alen;
+
+				_tnode *newLeafNode = new _tnode(start_pos, _end);
+				_tnode *newInternalNode = new _tnode(currentStart, new _End(currentStart+alen-1));
+				newInternalNode->nodeMark = -1;
+
+				newInternalNode->addChild(input_vect[alen+newInternalNode->_start], currNode);
+				newInternalNode->addChild(input_vect[start_pos], newLeafNode);
+				// replacing newInternal node with previous node
+				anode->childs[input_vect[newInternalNode->_start]] = newInternalNode;
+				if(lastIntNode){
+					lastIntNode->suffix_link = newInternalNode;
+				}
+				newInternalNode->suffix_link = root;
+				lastIntNode = newInternalNode;
+
+
+				if(anode == root){
+					//cout<<"root"<<endl;
+					alen--;
+					aedge++;
+				}
+				else{
+					//cout<<"not root"<<endl;
+					anode = anode->suffix_link;
+				}
+
+				_suffixToBeAdded--;
 			}
 
 		}
+		cout<<" active edge "<<aedge<<"alen "<<alen<<endl;
+	}
+}
+
+void printTreeNodes(_tnode *tree){
+
+	if(tree!=root){
+		cout<<tree->_start<<" "<<tree->_end->end_val;
+		if(tree->nodeMark==-1) cout<<" internal"<<endl;
+		else cout<<" leaf"<<endl;
+	}
+	MM::iterator it=tree->childs.begin();
+	if(it==tree->childs.end()) return;
+	for(;it!=tree->childs.end();++it){
+		printTreeNodes(it->second);
 	}
 }
 
@@ -104,6 +211,7 @@ _tnode * buildTree(_vint input_vect){
 
 	for(int i=0;i<input_vect.size();i++)
 	{
+		//cout<<"calling builds"<<endl;
 		buildPhases(i);
 	}
 
@@ -113,8 +221,8 @@ _tnode * buildTree(_vint input_vect){
 
 int main()
 {
-	cout<<"Ukkenon's program.."<<endl;
 
+	cout<<"Ukkenon's program.."<<endl;
 
 	char input_str[DATA_SIZE];
 	fgets(input_str,DATA_SIZE,stdin);
@@ -125,11 +233,24 @@ int main()
 	}while(token);
 
 
+	input_vect.push_back(100);		// adding unique last char
 	//testGetNextElement();
+	vPrint(input_vect);
 	_tnode *tree = buildTree(input_vect);
+
+	//_tnode *obj = tree->childs[1];
+
+	printTreeNodes(tree);
+	/*
+	MM::iterator it;
+	for(it=tree->childs.begin();it!=tree->childs.end();++it){
+		cout<<it->second->_start<<" "<<it->second->_end->end_val<<"internal marker"<<it->second->nodeMark<<endl;
+	}
+	*/
 
 	//calculateLeftDiverse(tree, input_vect);
 
 	//printSuffixes(tree, {});
+
 	return 0;
 }
--- suffixtree/Ukkenons.h
+++ suffixtree/Ukkenons.h
@@ -9,27 +9,35 @@
 using namespace std;
 typedef vector<int> _vint;
 typedef pair<int, int> _pair;
-
+struct _End{
+	int end_val;
+	_End(int a):end_val(a){}
+};
 struct TreeNode
 {
     //vector <TreeNode* > childs;
     unordered_map<int, TreeNode *> childs;
     TreeNode *suffix_link;
     _vint start_pos;
-    _pair edge_label;
+    //_pair edge_label;
+    int _start;
+    _End *_end;
     int nodeMark;
     bool leftDiverse;
     int leftChar;
-    TreeNode(){nodeMark=-1;edge_label={};leftDiverse=false;leftChar=-1;}
-    TreeNode(_pair label):edge_label(label){leftDiverse=false;leftChar=-2;}
+    TreeNode(){nodeMark=-1;leftDiverse=false;leftChar=-1;}
+    //TreeNode(_pair label):edge_label(label){leftDiverse=false;leftChar=-2;}
+    TreeNode(int st, _End *e):_start(st),_end(e){leftDiverse=false;leftChar=-2;}
     void addChild(int key, TreeNode *node){this->childs.insert({key,node});}
-    void updateLabel(_pair n){edge_label=n;}
+    //void updateLabel(_pair n){edge_label=n;}
 };
 typedef TreeNode _tnode;
-
+typedef unordered_map<int, TreeNode *> MM;
 struct ActivePoint
 {
 	_tnode *active_node;
 	int active_edge;
 	int active_len;
 };
+
+
