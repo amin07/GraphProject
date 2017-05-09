b6f5d7b41ec924de51947792c7bc1597ce5d3935
Amin <Amin@10.28.128.39>
2017-05-08 20:05:51 -0400

    Final commit: added functionality for running yeast data set

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # GraphProject
 This project searches hierarchical structure in sequential data
+The program is developed using C++11 and Python.
+Ukkonen's algorithm is developed in C++ and then called as a process from python module
+Python is used to develop actual DAG 
+
+Build Instructions:
+
+1. First need to build Ukkonen's algorithm implementation which is in sufixtree directory
+2. a Makefile is provided so running make command should be enough.
+3. System must have C++ 11
+4. The python module digraph.py can be run in python interpreter using python 3.5
+5. The code is written so that it can be run on three different data sets (simple input, yeast & igem)
+6. Proper line should be uncommented in the beginning of main and __init__ to run correct data set in digraph.py
+ 
--- digraph.py
+++ digraph.py
@@ -5,12 +5,14 @@ from sys import stdin
 import networkx as nx
 from _thread import _count
 import matplotlib.pyplot as plt
-
+from operator import itemgetter
+import time
 
 class CoreDAG(object):
     __intSequences = []             # contains converted string in int sequence form
     __symDic = {} 
     __adjList = {}
+    __adjListInv = {}
     __sequenceToSuffix = [] 
     __lexisGraph = nx.MultiDiGraph()
     __nodeString = {}               # string on each node
@@ -20,15 +22,21 @@ class CoreDAG(object):
     __uniqueSymbolids = []
     __newSymbol = 0
     
+    __nodeFpaths = {}              # contains (forw,back) path count as a tuple
+    __nodeBpaths = {}
+    __nodeScore = {}                # calculated score from path count
     # non private variables for printing purpose
     _uniqueSymCnt = 0
     _totSymCnt = 0
     
     
     def __init__(self, textData):
-        tData = self.dataSetup(textData, 'igem');
+        
+        # uncomment one, which you want to run. should be similar as in main
+        
+        #tData = self.dataSetup(textData, 'igem');
         #tData = self.dataSetup(textData, 'simple_input');
-        #tData = self.dataSetup(textData, 'yeast');
+        tData = self.dataSetup(textData, 'yeast');
 
         # this section was for preparing data, but they now transfered to dataSetup method
 #         _charDic = {}
@@ -111,16 +119,43 @@ class CoreDAG(object):
             _counter = 1
             _processedSeq = ''
             for i in range(len(tData)):
+                self._totSymCnt += 1
                 if tData[i] not in _countDic:
                     _countDic[tData[i]] = _counter
                     _charDic[_counter] = tData[i]
                     _counter +=1
+                    self._uniqueSymCnt += 1
                 _processedSeq += str(_countDic[tData[i]]) + ' '
             self.__symDic = _charDic
             self.__intSequences = _processedSeq
             self.__newSymbol = _counter
         elif dataType=='yeast':
             print ('yeast processing')
+            _charDic = {}
+            _countDic = {}
+            _counter = 1
+            _processedSeq = ''
+            sepflag = False
+            for seq in tData.split('\n'):
+                if sepflag == True:
+                # this code is for multiple targets handling
+                    _processedSeq += str(_counter) + ' '
+                    self.__uniqueSymbols.append(_counter);
+                    _counter += 1
+                sepflag = True
+            
+                for bb in seq:
+                    self._totSymCnt += 1
+                    if bb not in _countDic:
+                        _countDic[bb] = _counter
+                        _charDic[_counter] = bb
+                        _counter +=1
+                        self._uniqueSymCnt += 1
+                    _processedSeq += str(_countDic[bb]) + ' '
+                    
+            self.__symDic = _charDic
+            self.__intSequences = _processedSeq
+            self.__newSymbol = _counter
             
         return tData  
     def getTheGraph(self):
@@ -294,41 +329,118 @@ class CoreDAG(object):
                                maxGainRp = list(map(int, rp.split()))
                                maxGainRpOcss = rpoccssorted
             # no candidate repeats found of len more than 1   
-            if maxGain<0:
+            print ("max gain {0}".format(maxGain))
+            if maxGain<=0:
                 break 
             self.updateNodeTrack(maxGainRp,maxGainRpOcss)
             
-            #print (maxGain)
+            
             #print (maxGainRp)
         self.constructDAG()
         self.stringOnEachNodeUpdated(0)
         print ("print string on each node..")
         print (self.__nodeString)
-
+    
+    def forward_path_calc(self, vertex):
+        if vertex==0:
+            return
+        for cds in self.__adjListInv[vertex]:
+            self.__nodeFpaths[cds] += 1
+            self.forward_path_calc(cds)
+    
+    def backward_path_calc(self, vertex):
+        if self.__lexisGraph.out_degree(vertex)==0:
+            return
+        for cds in self.__adjList[vertex]:
+            self.__nodeBpaths[cds] += 1
+            self.backward_path_calc(cds)
+    
+    def calculateNodeScore(self, outFile):
+        print ("calculating node score...")
+        node_list = self.__lexisGraph.nodes()
+        print (node_list)
+        for nd in node_list:
+            self.__adjListInv[nd] = ([])
+            self.__nodeFpaths[nd] = 0
+            self.__nodeBpaths[nd] = 0
+            self.__nodeScore[nd] = 0
+        for nd in node_list:
+            if nd !=0:
+                for ed in self.__adjList[nd]:
+                    self.__adjListInv[ed].append(nd)
+            else :
+                for tars in self.__adjList[nd]:
+                    for ed in tars:
+                        self.__adjListInv[ed].append(nd)
+        for nd in node_list:
+#            print ("calling for node {0}, indegree {1}, outdegree {2}".format(nd, self.__lexisGraph.in_degree(nd),self.__lexisGraph.out_degree(nd)))
+            if self.__lexisGraph.out_degree(nd)==0:
+#                print ("calling for node {}".format(nd))
+                self.forward_path_calc(nd)
+                #print (self.__nodeFpaths)
+        
+        for tars in self.__adjList[0]:
+            for ed in tars:
+                self.__nodeBpaths[ed] += 1
+                self.backward_path_calc(ed)
+        
+        
+        # actual score calculation
+        
+        for nd in node_list:
+            if self.__lexisGraph.out_degree(nd)==0:
+                self.__nodeScore[nd] = self.__nodeBpaths[nd]
+            elif nd!=0:
+                self.__nodeScore[nd] = self.__nodeBpaths[nd] * self.__nodeFpaths[nd]                   
+#         print (self.__adjList)
+#         print (self.__adjListInv)
+#         print (self.__nodeFpaths)
+#         print (self.__nodeBpaths)
+#         print (self.__nodeScore)        
+        
+#        top_five_node = sorted(self.__nodeScore.items(), key=itemgetter(1))[:5]
+        
+        print (self.__nodeScore)
+        x = self.__nodeScore.items()
+        y = sorted(x, key=lambda x: x[1],reverse=True)
+        for k in y:
+            if len(self.__nodeString[k[0]])>=2 and k[0]!=0:
+                outFile.write("node label {0}, count {1}\n".format(self.__nodeString[k[0]],k[1]))
+        
+        #for tops in top_five_node:
+        #   print ("node label: {0}, score: {1}".format(self.__nodeString[tops],top_five_node[tops]))    
+        
+        
+start_time = time.time()
+        
 if __name__ == "__main__":
     
-    # need to convert sequence chars into integer sequence so that a new symbol can be added always
-    
-    #cprocess.stdin.write("aabcdeaabxfd")
-    #while cprocess.poll()==None:
-    #    print (cprocess.communicate(input=)[0])      #communicates param passes string to stdin of child process
-    
-    fileStream = open('test_data/igem.txt', 'r')
+    # uncomment one, which you want to run. should be similar as in __init__
+    fileStream = open('test_data/yeastProteins1K.txt', 'r')
     #fileStream = open('test_data/input.txt', 'r')
+    #fileStream = open('test_data/igem.txt', 'r')
+    
     g = CoreDAG(fileStream)
     g.printSeq()
-    g.lexisFunc() 
+    g.lexisFunc()
+     
     G = g.getTheGraph()
     print ("number of nodes {0}, number of edges {1}".format(G.number_of_nodes(), G.number_of_edges()))
     print ("number of unique symbols in whole text {0}".format(g._uniqueSymCnt))
     print ("number of total symbols in whole text {0}".format(g._totSymCnt))
-#     nodeLabels = g.getNodeLabels()
-#     print ([len(i) for i in nodeLabels.values()])
+    print ("Program Execution time: %s" % (time.time() - start_time))
+    nodeLabels = g.getNodeLabels()
+    print ([len(i) for i in nodeLabels.values()])
+    
+    outFile = open('output/outputYeast.txt','w')
+    g.calculateNodeScore(outFile)
+    
+    
 #     pos = nx.spring_layout(G)
 #     nx.draw_networkx_nodes(G, pos, node_size=[300*len(i) for i in nodeLabels.values()])
 #     nx.draw_networkx_edges(G, pos)
 #     nx.draw_networkx_labels(G, pos, nodeLabels)
     #nx.draw_networkx_edges(G, pos, edgelist=black_edges, arrows=False)
-    #plt.show()
+#    plt.show()
      
     
